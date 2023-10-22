9b966f2fa5e4074ff0356d326ac4e6dfab77f583
Laviel <fafansin@gmail.com>
2023-10-22 10:37:44 +0300

    Created 9 buttons with the proper layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from './Square';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Square />
     </div>
   );
 }
